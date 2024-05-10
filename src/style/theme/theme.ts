05dfef80542208f4dc6d47815c2ff9b88589308f
shson <sunghyun7895@naver.com>
2024-05-10 13:34:08 +0900

    nomflix 개발 v1.0 - GlobalStyle 및 theme 인터페이스 구현

---
+++ src/style/theme/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme:DefaultTheme={
+    bgColor: "black"
+}
+
+export const lightTheme:DefaultTheme={
+    bgColor: "black"
+}
